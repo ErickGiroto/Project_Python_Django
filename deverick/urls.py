f6f2f8dbf26ac9d71530f23b0d025c52db6670ba
ErickGiroto <erick_giroto@hotmail.com>
2025-06-17 20:44:07 -0300

    Removido Contato e Sobre html

--- deverick/urls.py
+++ deverick/urls.py
@@ -1,14 +1,13 @@
 # Arquivo criado para gerencias as páginas Urls
 
 from django.urls import path
-from deverick.views import home, sobre, contato
+from deverick.views import home
 
 # Cliente(Pede) < (Server)Responde
 # Http Request < Http Response
 
 urlpatterns = [
     path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+ 
 
 ]
--- deverick/views.py
+++ deverick/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
     return render(request, 'page/home.html') # Redenizar o arquivo HTML com o 'render'
 
-def sobre(request):
-    return render(request, 'page/sobre.html')
-
-def contato(request):
-    return render(request, 'page/contato.html')
