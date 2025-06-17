72cf86b95564a0f847bd89a994c053c8d39c1f38
ErickGiroto <erick_giroto@hotmail.com>
2025-06-17 19:47:11 -0300

    Buscando as Urls dentro dos Apps

--- projeto_python/urls.py
+++ projeto_python/urls.py
@@ -15,28 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include # Incluindo as urls dos Apps
 
 # Cliente(Pede) < (Server)Responde
 # Http Request < Http Response
-def home(request):
-    return HttpResponse('HOME')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', include('deverick.urls') ) # Buscando as urls dentro do Apps 'DevErick'
+
 
 ]
-
-# Proxima etapa é criar APPs para gerencias as páginas!
-# É inviavél ficar adicionando path e def para criação de páginas!
