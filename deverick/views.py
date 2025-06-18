cfb4d654534c444d8f23cef6386e0c7a7fd88a51
ErickGiroto <erick_giroto@hotmail.com>
2025-06-17 21:12:32 -0300

    Quebrando o html em duas partes, pages e partials, ou seja, dividiou o html!

--- deverick/views.py
+++ deverick/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'page/home.html') # Redenizar o arquivo HTML com o 'render'
+    return render(request, 'page/pages/home.html', context= { 
+        'name' : 'Erick Giroto',
+        })
 
+# Redenizar o arquivo HTML com o 'render' e Define uma Variavel 'name'
